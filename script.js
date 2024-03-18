8bf36a9f3b7a4b801196f945a99d8f6a671e53c3
Farouk Khaled <80914530+FarouqKhaled17@users.noreply.github.com>
2024-03-18 09:54:57 +0200

    the project has finished..!👌

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
+var siteNameInput = document.getElementById("siteName");
+var siteUrlInput = document.getElementById("siteURL");
+var bookContainer = [];
 
+if (localStorage.getItem("books") != null) {
+    bookContainer = JSON.parse(localStorage.getItem("books"));
+    displayBook();
+
+}
+
+function addBook() {
+    event.preventDefault();
+    var book = {
+        siteName: siteNameInput.value,
+        siteUrl: siteUrlInput.value
+    }
+    if (siteNameInput.value === "" || siteUrlInput.value === "") {
+        alert("Please fill in the form");
+    }
+    else {
+        bookContainer.push(book);
+        localStorage.setItem("books", JSON.stringify(bookContainer));
+        displayBook();
+    }
+    console.log(book);
+}
+
+function displayBook() {
+    var cartona = "";
+    for (var i = 0; i < bookContainer.length; i++) {
+        cartona += `<tr>
+        <td> ${i + 1}</td>
+        <td>${bookContainer[i].siteName}</td>
+        <td><a href="https://${bookContainer[i].siteUrl}" target="_blank" class="btn btn-primary"><i class="fa-solid fa-eye"></i> Visit</a></td>
+        <td><button class="btn btn-danger" onclick="deleteProduct(${i})"><i class="fa-solid fa-trash-can"></i> Delete</button></td>
+        </tr>`
+    }
+    document.getElementById("tableBody").innerHTML = cartona;
+}
+
+function deleteProduct(index) {
+    bookContainer.splice(index, 1);
+    localStorage.setItem("books", JSON.stringify(bookContainer));
+    displayBook();
+}
